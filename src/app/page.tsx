dcc07c1560c73b7e75ff18d222df68f151f23177
Hamzah Al Farisi <fariez18@outlook.com>
2024-07-18 23:50:42 +0700

    chore: Update weather API key and background color
    Update the weather API key in the .env.example file to remove the hardcoded value. Also, change the background color of the page to a light blue shade.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
       </div>
     )
   return (
-    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
+    <div className="flex flex-col gap-4 bg-[#7bedfe] min-h-screen ">
       <Navbar location={data?.city.name} />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4 ">
         {/* today data  */}
